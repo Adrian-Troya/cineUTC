3902cddd7b74079a9dca832f9255c201b751951e
WAdrian Troya <adrianttroya@gmail.com>
2024-08-01 10:31:01 -0500

    cambios

--- Aplicaciones/Cartelera/models.py
+++ Aplicaciones/Cartelera/models.py
@@ -13,7 +13,7 @@ class Genero(models.Model):
         return fila.format(self.id,self.nombre,self.descripcion)    
     
 class Director(models.Model):
-    id=models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     dni = models.CharField(max_length=20)
     apellido = models.CharField(max_length=50)
     nombre = models.CharField(max_length=50)
@@ -22,8 +22,8 @@ class Director(models.Model):
     portada = models.ImageField(upload_to='directores/', blank=True, null=True)
 
     def __str__(self):
-        fila="{0}: {1} - {2}  {3} - {4} - {5}"
-        return fila.format(self.id,self.dni,self.apellido,self.nombre,self.estado)
+        fila = "{0}: {1} - {2}  {3} - {4}"
+        return fila.format(self.id, self.dni, self.apellido, self.nombre, self.estado)
 
 class Pais(models.Model):
     id=models.AutoField(primary_key=True)
