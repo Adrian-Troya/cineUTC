c1cd31e0c6cf865d1c8d2680c0952e312c3eddd7
WAdrian Troya <adrianttroya@gmail.com>
2024-08-01 11:10:36 -0500

    cambios

--- cineUTC/settings.py
+++ cineUTC/settings.py
@@ -12,24 +12,23 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-# IMPORTAR PAQUETE BDD
+
+#IMPORTAR PAQUETE BDD
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@+y6qgc_5d*-=cp78j!1*glhnt!re@zp6xnk69fk%-w6pr#g3*'
+SECRET_KEY = 'django-insecure-=w-(h7ied$je9cz&7^-e$a^h1shdan-if)-l0pdij-hp2!5!cy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'cineutc-qstg.onrender.com').split(',')
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'cineutc-b1lz.onrender.com').split(',')
 
 # Application definition
 
@@ -45,8 +44,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #AÑADIR LA LIBRERIA
-    'whitenoise.middleware.WhiteNoiseMiddleware', #lo que se agrego
+    #AÑADIR LIBRERIA
+    'whitenoise.middleware.WhiteNoiseMiddleware', #lo que se agrego
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,15 +81,17 @@ WSGI_APPLICATION = 'cineUTC.wsgi.application'
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'bddCineUTC.db',
+#         'NAME':'bddCineUTC.db',
 #     }
 # }
 
-#BDD POSTGRESQL
 DATABASES = {
     # 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -126,11 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
+#Configurando de la carpeta para gestionar archivos estatico
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS= (os.path.join(BASE_DIR, 'cineUTC/static'),)
-
-#VALIDACION
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'cineUTC/static/'),)
+# VALIDACION
 if not DEBUG:
     STATICFILES_ROOT = os.path.join(BASE_DIR,'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -138,15 +140,26 @@ if not DEBUG:
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR, 'cineUTC/media')
+#Configuracion de la carpeta para subir archivos dinamicos
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'cineUTC/media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#PARA ENVIAR CORREO ELECTRONICO
+#configurcion para gmail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'javierguala07@gmail.com'
-EMAIL_HOST_PASSWORD = 'jefo xlvq lylq vtoq'
+
+EMAIL_HOST_USER = 'wilmer.troya1680@utc.edu.ec'  # Tu dirección de correo de Gmail
+EMAIL_HOST_PASSWORD = 'upkuzuvshfolcjwv'  # Contraseña de aplicación generada desde tu cuenta de Gmail
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER  # La dirección de correo desde la cual se envían los correos
+
+
+
+#clave de aplicación: upku zuvs hfol cjwv
+
+
+# settings.py
+
